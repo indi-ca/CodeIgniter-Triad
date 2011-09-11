6f931ec70fa7a43eae3332bc933a87ab0fcdf5a7
Indika <indikap@yahoo.com>
2011-09-11 22:48:40 +0700

    datamappers works fine with phpunit

--- tests/libs/SomeLibTest.php
+++ tests/libs/SomeLibTest.php
@@ -11,12 +11,13 @@ class SomeLibTest extends CIUnit_TestCase
 		// Set up fixtures to be run before each test
 		
 		// Load the tested library so it will be available in all tests
-		//$this->CI->load->library('example_lib', '', mylib);
+		$this->CI->load->library('test_library');
+
 	}
 	
 	public function testMethod()
 	{
 		// Check if everything is ok
-		//$this->assertEquals(4, $this->CI->mylib->add(2, 2));
+		$this->assertEquals(1, $this->CI->test_library->test());
 	}
 }

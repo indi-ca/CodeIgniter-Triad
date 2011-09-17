3bb5263ede4bda12b6b14586ee384b26a7243913
Indika <indikap@yahoo.com>
2011-09-18 00:09:27 +0700

    hybrid controller added

---
+++ application/controllers/hybrid.php
@@ -0,0 +1,25 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+//include_once "application/controllers/../modules/modulea/controllers/albert.php";
+
+class Hybrid extends CI_Controller {
+
+	public function index()
+	{
+		//load partial views
+
+//		Datamapper::add_model_path( array( APPPATH.'modules/modulea', APPPATH.'modules/moduleb') );
+//		$users = new User();
+//		$users->get();
+//		foreach ($users as $user)
+//		{
+//			echo $user->description;
+//		}
+
+		$this->load->module('modulea/albert');
+		$this->load->view('hybrid/index');
+	}
+}
+
+/* End of file hybrid.php */
+/* Location: ./application/controllers/hybrid.php */
